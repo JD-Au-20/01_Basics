7c354c48a8c7dec9d27cd8cbc0d4b45ec8595dac
Jakob <jakob@example.com>
2025-11-12 16:20:08 +0100

    45 Aufgabe1 Done

---
+++ 45 Deno/Aufgabe_1_Password/index.js
@@ -0,0 +1,9 @@
+let password = "password";
+
+let input = prompt("What's the password");
+
+if (input == "password") {
+  console.log("Access granted");
+} else {
+  console.log("Access denied");
+}
