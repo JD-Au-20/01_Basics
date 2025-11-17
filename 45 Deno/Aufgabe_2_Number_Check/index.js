d29d332eb87f8f539461597f8a83efb3cabe4e09
Jakob <jakob@example.com>
2025-11-17 11:03:24 +0100

    2 Aufgabe Deno Number Check done

---
+++ 45 Deno/Aufgabe_2_Number_Check/index.js
@@ -0,0 +1,18 @@
+let a = 16;
+if (a > 0) {
+  console.log("The Number is positiv");
+} else {
+  console.log("The Number ins't positiv");
+}
+
+if (a < 0) {
+  console.log("The Number is negativ");
+} else {
+  console.log("The Number is positiv or 0");
+}
+
+if (a == 0) {
+  console.log("The Number is zero");
+} else {
+  console.log("The Number is not 0");
+}
