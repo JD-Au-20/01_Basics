a426c9b43610b43929b47cf5bd9be9c7a90bf011
Jakob <jakob@example.com>
2025-11-19 16:27:08 +0100

    45 Deno Aufgabe 6 Getränkeautomat done

---
+++ 45 Deno/Aufgabe_6_Getränkeautomat/index.js
@@ -0,0 +1,23 @@
+let selection = parseInt(
+  prompt("Which drink you would like: 1 Cola, 2 Watter, 3 Bier, 4 Tea")
+);
+
+if (selection == 1) {
+  console.log("You get a cola");
+}
+
+if (selection == 2) {
+  console.log("You get Watter");
+}
+
+if (selection == 3) {
+  console.log("You get Bier");
+}
+
+if (selection == 4) {
+  console.log("You get a Tea");
+}
+
+if (selection == 3) {
+  console.log("Thanks for your Besttelung");
+}
