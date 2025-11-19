61431ab9fba337b85f9d54c29e27ea06b3e83d99
Jakob <jakob@example.com>
2025-11-19 15:46:22 +0100

    45 Deno Aufgabe 4 Alter Done

--- 45 Deno/Aufgabe_4_Alter/index.js
+++ 45 Deno/Aufgabe_4_Alter/index.js
@@ -1,1 +1,11 @@
-let number1 = 
+let age = parseInt(prompt("How old are you"));
+
+if (age < 10) {
+  console.log("Yor are very young");
+} else if (age < 25) {
+  console.log("You are still young");
+} else if (age < 59) {
+  console.log("You are middleaged");
+} else if (age > 60) {
+  console.log("Yor're getting old");
+}
