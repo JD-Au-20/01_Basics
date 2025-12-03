d368da7d539907dae63fa37569ea3bec678879c0
Jakob <jakob@example.com>
2025-12-03 15:28:38 +0100

    45_Deno_6_Aufgabe_Getränkeautomat_Überarbeitet/Verbessert

--- 45 Deno/Aufgabe_6_Getränkeautomat/index.js
+++ 45 Deno/Aufgabe_6_Getränkeautomat/index.js
@@ -7,7 +7,7 @@ if (selection == 1) {
 }
 
 if (selection == 2) {
-  console.log("You get Watter");
+  console.log("You get Water");
 }
 
 if (selection == 3) {
@@ -19,5 +19,17 @@ if (selection == 4) {
 }
 
 if (selection == 3) {
-  console.log("Thanks for your Besttelung");
+  console.log("Thanks for your order");
+}
+
+if (selection == 4) {
+  console.log("Is it really that cold???");
+}
+
+if (selection == 2) {
+  console.log("It is very gesund");
+}
+
+if (selection == 1) {
+  console.log("Be careful there is much suggar in it");
 }
