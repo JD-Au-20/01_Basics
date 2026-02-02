4f0d1e5f0c0740cc6554a0c51d62cac51c610fe8
Jakob <jakob@example.com>
2026-02-02 11:25:52 +0100

    1 Versuch bei html

--- 70_ Arrays/Aufgabe_5_Find_the_Largest_or_Smallest_Number/index.js
+++ 70_ Arrays/Aufgabe_5_Find_the_Largest_or_Smallest_Number/index.js
@@ -1,9 +1,10 @@
-let arr = [87, 45, 9, 2, 34];
+let arr = [87, 97, 1021, 102, 34];
 let a = 0;
 
 for (let i = 0; i < arr.length; i++) {
   if (arr[i] > a) {
     a = arr[i];
-    console.log(a);
   }
 }
+
+console.log(a);
