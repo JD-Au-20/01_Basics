6902c8c344759cc46c60f83e5cc0ac88598dc622
Jakob <jakob@example.com>
2026-05-20 09:05:48 +0200

    2 Video fertig

---
+++ 03_Web/Übungen_CSS/01_Zugriff_JS_Html_2Vid/script.js
@@ -0,0 +1,8 @@
+const button = document.getElementById("button")
+
+const element = document.getElementById("content")
+
+button.addEventListener("click", function () {
+  
+  element.innerHTML = "Super"
+})
